71b8156368195701f00dea9e274b747f9d3cf175
evgene30 <evgene.fe@gmail.com>
2021-07-06 18:06:26 +0300

    add new index js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from "react-dom";
 import "./scss/index.scss";
-import App from "./App";
+import App from "./components/App";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App/>, document.getElementById("root"));
+
+
+
 
