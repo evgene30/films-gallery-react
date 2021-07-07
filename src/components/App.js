432acd6aa74533b51e498543fae12d14054b6bdf
evgene30 <evgene.fe@gmail.com>
2021-07-07 20:25:19 +0300

    fix render component

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,70 @@ import Main from "./Main/Main";
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            preloader: false,
+            itemsFilm: [],
+            error: null,
+        };
+    }
+
+
+    componentDidMount() {
+        const filmList = 'https://api.themoviedb.org/3/list/7095647?api_key=833e2dd8979208fbee927efb619ed90a&language=ru-RU';
+        const sortMassiveFilm = (array) => {
+            const massive = [];
+            for (let i = 0; i < Math.ceil(array.length / 20); i++) { // разбиваем исходный массив на вложенные массивы
+                // по 20 вложенных массивов
+                massive[i] = array.slice(i * 20, i * 20 + 20);
+            }
+            return massive;
+        }
+
+        fetch(filmList)
+            .then((response) => response.json())
+            .then(
+                (value) => {
+                    this.setState({
+                        preloader: true,
+                        itemsFilm: sortMassiveFilm(value.items),
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        preloader: true,
+                        error
+                    });
+                }
+            )
+    }
 
     render() {
-        return (
-            <div className="App">
-                <div id="preloader" className="visible"/>
-                <div className="container">
-                    <Header/>
-                    <Main/>
-                    <Footer/>
+        const {error, preloader, itemsFilm} = this.state;
+        if (error) {
+            return (
+                <div className="error-response">
+                    Ошибка: {error.message}
+                    <a href="/">
+                        <div className="button-reboot">Перезагрузить</div>
+                    </a>
                 </div>
-            </div>
-        );
+            );
+        } else if (!preloader) {
+            return (<div id="preloader" className="visible"/>);
+        } else {
+            console.log(itemsFilm)
+            return (
+                <div className="App">
+                    <div className="container">
+                        <Header/>
+                        <Main/>
+                        <Footer/>
+                    </div>
+                </div>
+            );
+        }
     }
 }
 
