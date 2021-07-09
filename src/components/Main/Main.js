04398ee45ad036abea980b0fc7e6dc32b344677c
evgene30 <evgene.fe@gmail.com>
2021-07-09 14:15:35 +0300

    all pagination

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -25,10 +25,22 @@ class Main extends Component {
     }
 
     handleVisualPagination = (index) => {
-        if (Number(!index) === 1 || index >= 19) {
-            return {display: "flex"};
-        } else {
-            return {display: "none"}
+        const selectPage = Number(this.state.filmPage);
+        switch (selectPage >= 1 && selectPage <= 20) {
+            case (selectPage >= 1 && selectPage <= 4 && index > 0 && index < 5):
+                return {display: "flex"};
+            case (selectPage >= 5 && selectPage < 8 && index > 2 && index < 8):
+                return {display: "flex"};
+            case (selectPage >= 8 && selectPage < 11 && index > 5 && index < 11):
+                return {display: "flex"};
+            case (selectPage >= 11 && selectPage < 14 && index > 8 && index < 14):
+                return {display: "flex"};
+            case (selectPage >= 14 && selectPage < 17 && index > 11 && index < 17):
+                return {display: "flex"};
+            case (selectPage >= 17 && selectPage <= 20 && index > 14 && index < 19):
+                return {display: "flex"};
+            default:
+                return {display: "none"}
         }
     }
 
@@ -72,6 +84,9 @@ class Main extends Component {
                 <div className="pagination-block">
                     <ul className="pagination" id="pagination">
                         <li tabIndex="0" className="nextclick mt" onClick={this.handleClick}>Prev</li>
+                        <li tabIndex="0" className={this.state.filmPage === 1 ? "active" : ""}
+                            onClick={this.handleClick}>1
+                        </li>
                         {massiveFilms.map((item, index) => {
                             return (<li tabIndex="0" key={index}
                                         className={index + 1 === this.state.filmPage ? "active" : ""}
@@ -81,6 +96,9 @@ class Main extends Component {
                                 {index + 1}
                             </li>);
                         })}
+                        <li tabIndex="0" className={this.state.filmPage === 20 ? "active" : ""}
+                            onClick={this.handleClick}>20
+                        </li>
                         <li tabIndex="0" className="nextclick mt" onClick={this.handleClick}>Next</li>
                     </ul>
                 </div>
