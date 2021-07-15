8f5230f26463d3651b87cff290ad455da28f0f38
evgene30 <evgene.fe@gmail.com>
2021-07-15 22:44:54 +0300

    fix bug

--- src/components/Main/Register/Register.js
+++ src/components/Main/Register/Register.js
@@ -1,20 +1,19 @@
 import closeImg from "../../../assets/png/close.png";
-import {useHistory} from "react-router-dom";
-import {useState} from "react";
-import "./Register.scss"
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import "./Register.scss";
 import RegisterNewUser from "./RegisterNewUser";
-import Json from "../../../dummy_data/users.json"
+import Json from "../../../dummy_data/users.json";
 
 const Register = (props) => {
-    const messageEmail = [].join();
-    const messagePass = [].join();
-
-    const {hendleVerificationUser, handleMarkCard} = props;
+    const { hendleVerificationUser, handleMarkCard } = props;
 
     const [state, setState] = useState({
         form: true,
         email: "",
         pass: "",
+        mesEmail: "",
+        mesPass: "",
     });
     const history = useHistory();
 
@@ -24,9 +23,8 @@ const Register = (props) => {
     };
 
     const handleClickRegister = () => {
-        setState({form: !state.form})
-
-    }
+        setState({ form: !state.form });
+    };
     const handleSubmit = (event) => {
         event.preventDefault();
 
@@ -36,22 +34,26 @@ const Register = (props) => {
                     hendleVerificationUser(item.status);
                     history.push("./");
                 }
-                messageEmail.push("Не верный пароль");
+                setState({ form: true, pass: "", mesPass: "Не верный пароль" });
             } else {
-                messagePass.push("Не верный email")
+                setState({
+                    form: true,
+                    email: "",
+                    pass:"",
+                    mesEmail: "Ваш email не зарегистрирован",
+                });
             }
-        })
-
-    }
+        });
+    };
 
     const handleInputChange = (event) => {
         const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const value =
+            target.type === "checkbox" ? target.checked : target.value;
         const name = target.name;
         setState((prevState) => ({
             ...prevState,
-            [name]:
-            value
+            [name]: value,
         }));
     };
 
@@ -62,37 +64,60 @@ const Register = (props) => {
                 alt="Close"
                 src={closeImg}
                 onClick={handleClickClose}
-                style={{height: "40px", width: "40px"}}
+                style={{ height: "40px", width: "40px" }}
             />
-            {
-                state.form &&
-                <form className="form_auth_style" id="regform" onSubmit={handleSubmit}>
-                    <label htmlFor="auth_email"
-                           id="labelmail">{messageEmail ? messageEmail : "Введите Ваш email:"}</label>
-                    <input type="email" name="email" placeholder="@email" required id="auth_email"
-                           onChange={handleInputChange} value={state.email}/>
-                    <label htmlFor="auth_pass" id="labelinput">Введите Ваш пароль:</label>
-                    <input type="password" name="pass" placeholder="password" required id="auth_pass"
-                           onChange={handleInputChange} value={state.pass}/>
+            {state.form && (
+                <form
+                    className="form_auth_style"
+                    id="regform"
+                    onSubmit={handleSubmit}
+                >
+                    <label htmlFor="auth_email" id="labelmail">
+                        {state.mesEmail ? state.mesEmail : "Введите Ваш email:"}
+                    </label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="@email"
+                        required
+                        id="auth_email"
+                        onChange={handleInputChange}
+                        value={state.email}
+                    />
+                    <label htmlFor="auth_pass" id="labelinput">
+                    {state.mesPass ? state.mesPass : "Введите ваш пароль:"}
+                    </label>
+                    <input
+                        type="password"
+                        name="pass"
+                        placeholder="password"
+                        required
+                        id="auth_pass"
+                        onChange={handleInputChange}
+                        value={state.pass}
+                    />
 
-                    <button className="form_auth_button" type="submit"
-                            id="auth_aut"
-                    >Войти
+                    <button
+                        className="form_auth_button"
+                        type="submit"
+                        id="auth_aut"
+                    >
+                        Войти
                     </button>
 
-                    <div className="form_auth_register" name="form_auth_submit"
-                         id="auth_reg"
-                         onClick={handleClickRegister}
-                    >Зарегистрироваться
+                    <div
+                        className="form_auth_register"
+                        name="form_auth_submit"
+                        id="auth_reg"
+                        onClick={handleClickRegister}
+                    >
+                        Зарегистрироваться
                     </div>
                 </form>
-            }
-            {
-                !state.form && <RegisterNewUser/>
-            }
+            )}
+            {!state.form && <RegisterNewUser />}
         </section>
-    )
-}
-
+    );
+};
 
 export default Register;
