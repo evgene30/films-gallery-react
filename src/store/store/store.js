da4c5df4e694fffc55461a8eca83cebed27c5520
evgene30 <evgene.fe@gmail.com>
2021-08-01 17:50:41 +0300

    check tools

--- src/store/store/store.js
+++ src/store/store/store.js
@@ -3,8 +3,7 @@ import {createStore, combineReducers, applyMiddleware, compose} from "redux";
 import logger from "../middleware/logger";
 import thunk from "redux-thunk";
 
-const composedEnhancer = compose(applyMiddleware(logger, thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composedEnhancer = compose(applyMiddleware(logger, thunk));
 const comboReducer = combineReducers({
     stateApp: Reducer,
 });
